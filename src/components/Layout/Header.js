7c963db0f0c12baf987696f7928d25dfb9051172
tejashwinikr <ahwiniteju@gmail.com>
2022-11-12 20:10:15 +0530

    upto video

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header= props =>{
     return <React.Fragment>
         <header className={classes.header}>
             <h1>Food Delivery App</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={classes['main-image']}>
            <img src={meals} alt ="meals"/> 
